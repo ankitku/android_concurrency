fef5a808aacad3a55ebca2daa981624427f9891f
Ankit Kumar <ankitkumar.itbhu@gmail.com>
2017-09-19 19:27:18 +0530

    removed script from inside project

--- project/download_dependencies.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/Sable/jasmin
-git clone https://github.com/Sable/heros
-git clone https://github.com/Sable/soot
-git clone https://github.com/secure-software-engineering/soot-infoflow
-git clone git@github.com:secure-software-engineering/soot-infoflow-android.git
-
